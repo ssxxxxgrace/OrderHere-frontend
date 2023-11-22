59025a9ad36aaeccd408d52abcd04464f56e204f
cansibi <69489820+cansibi@users.noreply.github.com>
2023-11-22 23:41:21 +1030

    add carousel and category

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import ThreeColumnsLayout from '../layout/ThreeColumnsLayout';
 import FoodItemsList from '../components/DishList/FoodItemsList';
+import Carousel from '../components/Carousel/Carousel';
+import Category from '../components/Category/Category';
 
 const Index = () => {
   return (
     <ThreeColumnsLayout>
       {/* Hello World */}
+      <Carousel />
+      <Category />
       <FoodItemsList />
     </ThreeColumnsLayout>
   );
