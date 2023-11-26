c1d2da50d94b9801f5a87197fbcf4f7af509efa2
LinW <104260056+wenlinwwww@users.noreply.github.com>
2023-11-26 23:58:26 +1100

    Oj 81 order history (#41)
    * create order history page
    
    * fetch restaurant name
    
    * add order history into profile
    
    * fixed button issue
    
    * fix api
    
    ---------
    
    Co-authored-by: Alan <prossliu@gmail.com>

--- services/orderService.js
+++ services/orderService.js
@@ -5,3 +5,5 @@ export const placeOrder = (orderData) =>
         method: 'POST',
         data: orderData,
     });
+
+export const getUserOrder = (userId) => http(`/v1/public/orders/user/${userId}`, { method: 'GET' });
--- store/reducers/index.js
+++ store/reducers/index.js
@@ -4,6 +4,7 @@ import cartReducer from './cartReducer';
 import restaurantReducer from './restaurantReducer';
 import deliveryReducer from './deliveryReducer';
 import filterReducer from './filterReducer';
+import historyReducer from './historyReducer';
 
 export default combineReducers({
   sign: signReducer,
@@ -11,4 +12,5 @@ export default combineReducers({
   restaurant: restaurantReducer,
   delivery: deliveryReducer,
   filter: filterReducer,
+  history: historyReducer,
 });
