9c74de55572ada81512459782489702cde71a45f
ZacharyZ <117606781+Zachary1319@users.noreply.github.com>
2023-11-30 18:14:43 +1100

    change the image url to upload image (#50)
    Co-authored-by: SteveGuoh <127906105+SteveGuoh@users.noreply.github.com>

--- services/Dish.js
+++ services/Dish.js
@@ -2,11 +2,19 @@ import http from '../utils/axios';
 
 export const getDishes = () => http(`/v1/public/dish/1`, { method: 'GET' });
 
-export const postDishes = (dishData) => http(`/v1/public/dish`, {
+export const postDishes = (dishData) => {
+  const formData = new FormData();
+  for (const key in dishData) {
+    if (key === 'imageFile' && dishData[key]) {
+      formData.append(key, dishData[key], dishData[key].name);
+    } else {
+      formData.append(key, dishData[key]);
+    }
+  }
+
+  return http(`/v1/public/dish`, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: JSON.stringify(dishData)
+    data: formData
   });
+};
 
